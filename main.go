8006ccd3e1bb151e8a19de533e256f5c94a29128
Hudayberdyyev <hudayberdyyev.ahmet@gmail.com>
2021-11-24 19:50:43 +0500

    parse csv file

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
-func main()
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readCsvFile(filePath string) [][]string {
+	// Load a csv file.
+	f, _ := os.Open(filePath)
+
+	// Create a new reader.
+	r := csv.NewReader(f)
+	r.FieldsPerRecord = -1
+	r.LazyQuotes = true
+	r.Comma = ';'
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+
+	return records
+}
+
+func main() {
+	records := readCsvFile("/Users/ahmet/Desktop/all_data.csv")
+	fmt.Println(cap(records), " records")
+}
